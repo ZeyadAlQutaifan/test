7472cbde15ac46c72f291ce720f5851021273d49
zeyad alqutaifan <zeyada.alqutaifan@gmail.com>
2023-09-08 16:28:40 +0100

    add new Model class User.java

---
+++ app/src/main/java/com/example/githubtest/User.java
@@ -0,0 +1,13 @@
+package com.example.githubtest;
+
+public class User {
+    private String userName  ;
+    private int age ;
+    private  Long id  ;
+
+    public User(String userName , int age , Long id){
+        this.age = age  ;
+        this .id = id ;
+        this.userName = userName ;
+    }
+}
