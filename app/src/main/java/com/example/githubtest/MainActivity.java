e0380d0d7c623ffcdc069f8e1541a5904b7f3f3d
WaseemNaji11 <144339070+WaseemNaji11@users.noreply.github.com>
2023-09-08 18:18:18 +0300

    waseem

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Hello, World");
-        System.out.println("waseem");
+
+        System.out.println("w");
+        System.out.println("zeyad");
     }
 }
